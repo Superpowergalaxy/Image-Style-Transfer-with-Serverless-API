d16cbb5c1bea2863da0670375807467a2ebe4894
Jingyu Su <jingyusu@Jingyus-Mac-mini.local>
2020-04-11 10:24:31 -0400

    add file

---
+++ credentials.py
@@ -0,0 +1,5 @@
+# Variables that contains the user credentials to access AWS API 
+ACCESS_ID = ""
+ACCESS_KEY=""
+
+
--- lambda_API_GateWay.py
+++ lambda_API_GateWay.py
@@ -1,18 +1,54 @@
 import json
 import base64
 import boto3
-BUCKET_NAME = 'YOUR_S3_BUCKET_NAME_HERE'
+import datetime
+from credentials import ACCESS_ID, ACCESS_KEY
+BUCKET_NAME = 'image-style-transfer-jingyu'
 
 def lambda_handler(event, context):
+    # safe to S3
     file_content = base64.b64decode(event['content'])
-    file_path = 'YOUR_FILE_PATH_HERE'
-    s3 = boto3.client('s3')
+    file_path = event['name'] + str(datetime.datetime.now().timestamp())
+    s3_client = boto3.client('s3',
+        aws_access_key_id=ACCESS_ID,
+        aws_secret_access_key=ACCESS_KEY)    
     try:
-        s3_response = s3.put_object(Bucket=BUCKET_NAME, Key=file_path, Body=file_content)
+        s3_response = s3_client.put_object(Bucket=BUCKET_NAME, Key=file_path, Body=file_content)
     except Exception as e:
         raise IOError(e)
-    return {
-        'statusCode': 200,
-        'body': {
-            'file_path': file_path
+    
+    # create sqs message
+    QueueName = 'sqs'
+    # Send some SQS messages
+    msg = send_sqs_message(QueueName,file_path)
+    if msg is not None:
+        logging.info(f'Sent SQS message ID: {msg["MessageId"]}')
+
+
+    return { 'statusCode': 200,
+        'body': { 'file_path': file_path
+            }
         }
+
+def send_sqs_message(QueueName, msg_body):
+    """
+
+    :param sqs_queue_url: String URL of existing SQS queue
+    :param msg_body: String message body
+    :return: Dictionary containing information about the sent message. If
+        error, returns None.
+    """
+
+    # Send the SQS message
+    sqs_client = boto3.client('sqs',
+        aws_access_key_id=ACCESS_ID,
+        aws_secret_access_key=ACCESS_KEY)
+    sqs_queue_url = sqs_client.get_queue_url(
+    QueueName=QueueName)['QueueUrl']
+    try:
+        msg = sqs_client.send_message(QueueUrl=sqs_queue_url,
+                                      MessageBody=json.dumps(msg_body))
+    except ClientError as e:
+        logging.error(e)
+        return None
+    return msg
